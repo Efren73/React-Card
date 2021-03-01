0a421bdba266cfa52996b01a48678af5ddca44ce
Efren73 <A01276266@itesm.mx>
2021-02-28 20:45:13 -0600

    Card con estilos terminada

--- src/Mycomponent.js
+++ src/Mycomponent.js
@@ -12,6 +12,7 @@ function Mycomponent ( {name: nombre, mail: correo}) {
   let user = {insta: 'efren_aldana'};
   return(
     <div className = "card">
+      <h1>CARD</h1>
       <img id="yo" src="https://pbs.twimg.com/profile_images/1278682744139190273/oRcAsOSm.jpg" />
       <p>{`Nombre: ${nombre}`}</p>  {/*Este ejeemplo es con Destructuring*/}
       <p>{`Correo: ${correo}`}</p> 
